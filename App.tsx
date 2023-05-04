ee431468d3c9501117f0f0737a142b5f34075b61
lonewolf1503 <104578544+lonewolf1503@users.noreply.github.com>
2023-05-04 22:22:59 +0530

    home-screen compnent built

--- App.tsx
+++ App.tsx
@@ -8,17 +8,24 @@
 
 import React from 'react';
 import type {PropsWithChildren} from 'react';
-import {Text, View} from 'react-native';
+import {Text, View, SafeAreaView} from 'react-native';
 
 import {Provider} from 'react-redux';
 import {store} from './store';
+import HomeScreen from './screens/HomeScreen';
 
 export default function App() {
   return (
     <Provider store={store}>
-      <View>
-        <Text className="text-black flex-1 bg-color-white">App</Text>
-      </View>
+      <HomeScreen />
     </Provider>
   );
 }
+
+const styles = {
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+};
